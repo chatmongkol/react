4320d55546bfeafabe2bee5897b9c4aed229915e
chatmongkol <33957865+chatmongkol@users.noreply.github.com>
2018-08-05 15:47:26 +0700

    Create Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from '../src/components/Header';
+import MessageList from './components/MessageList';
 
 class App extends Component {
   render() {
@@ -10,9 +12,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          ทดสอบ อิอิ ...คิคิ....
-        </p>
+        <div className="container">
+        <Header title="Simple Firebase App" />
+        <MessageList />
+       </div>
       </div>
     );
   }
