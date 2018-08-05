fe3677c6d8b49f6ec195196955c44b8e894344fd
chatmongkol <33957865+chatmongkol@users.noreply.github.com>
2018-08-05 17:02:03 +0700

    Workshop Message
    ทำ Workshop Message เสร็จเรียบร้อย

--- src/App.js
+++ src/App.js
@@ -3,20 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import Header from '../src/components/Header';
 import MessageList from './components/MessageList';
+import MessageBox from './components/MessageBox';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    var config = {
+    apiKey: "AIzaSyB8C_2lWsSsJ9PqtbXJbjKYzcozABUe960",
+    authDomain: "workshop-message.firebaseapp.com",
+    databaseURL: "https://workshop-message.firebaseio.com",
+    projectId: "workshop-message",
+    storageBucket: "workshop-message.appspot.com",
+    messagingSenderId: "59181858300"
+
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
+      <div >
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="container">
-        <Header title="Simple Firebase App" />
-        <MessageList />
-       </div>
+        </div>
+      <div className="container">
+      
+      <h1 align="center" className="App-title">กล่องข้อความ</h1>
+      <div className="columns">
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageList db={firebase} />
+        </div>
       </div>
+      <div className="columns">
+        <div className="column is-3"></div>
+        <div className="column is-6">
+          <MessageBox db={firebase} />
+        </div>
+      </div>
+   </div>
+   </div>
     );
   }
 }
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,17 +1,50 @@
 import React, {Component} from 'react';
+import Message from './Message';
+import _ from 'lodash';
 class MessageList extends Component {
   constructor(props){
     super(props);
-    this.state  = {
+    this.state = {
       messages: []
-    }
+    };
+    let app = this.props.db.database().ref('messages');
+    app.on('value', snapshot => {
+      this.getData(snapshot.val());
+    });
   }
-  render(){
+  getData(values){
+    let messagesVal = values;
+    let messages = _(messagesVal)
+                    .keys()
+                    .map(messageKey => {
+                      let cloned = _.clone(messagesVal[messageKey]);
+                      cloned.key = messageKey;
+                      return cloned;
+                    }).value();
+    this.setState({
+      messages: messages
+    });
+  }
+  render() {
+    let messageNodes = this.state.messages.map((message) => {
+      return (
+        <div className="card">
+          <div className="card-content">
+            
+            <Message 
+  msgKey={message.key} 
+  message = {message.message} 
+  db={this.props.db}
+/>
+          </div>
+        </div>
+      )
+    });
     return (
       <div>
-        MessageList Component
+        {messageNodes}
       </div>
-    )
+    );
   }
 }
 export default MessageList
